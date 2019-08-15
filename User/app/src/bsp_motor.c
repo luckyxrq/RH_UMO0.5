db9b89a9fb4bea1214a6b0aae6d83e7ba416b0ac
luckyxrq <luckyxrq@163.com>
2019-08-15 16:11:56 +0800

    4路PWM输出不同的波形OK，经过逻辑分析仪的检测了

--- User/app/src/bsp_motor.c
+++ User/app/src/bsp_motor.c
@@ -1,6 +1,17 @@
 #include "bsp.h"
 
 
+/*
+	笔记：
+	硬件前提：bsp_InitTimer1(7199,0);10KHz
+	输出恒为0：TIM_SetCompare1(TIM1,0);
+	输出恒为1：TIM_SetCompare1(TIM1,7200);!!!千万别写成TIM_SetCompare1(TIM1,7199);;
+*/
+
+
+#define CONSTANT_HIGH_PWM      (7200)
+#define CONSTANT_LOW_PWM       (0)
+
 
 static void bsp_InitTimer1(uint16_t arr,uint16_t psc);
 
@@ -51,14 +62,14 @@ static void bsp_InitTimer1(uint16_t arr,uint16_t psc)
 	TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStructure); 
 	
 	/*
-	PWM一共有两种模式，PWM1模式：CNT<CRRx为有效电平。CNT>CRRx为无效电平。PWM2模式相反。
+	PWM一共有两种模式，PWM1模式：CNT<CRRx为有效电平（不包括等于）。CNT>=CRRx为无效电平。PWM2模式相反。
 	那么问题来了什么为有效电平？他又怎么确定？
 	它是由TIM_OCInitStructure.TIM_OCPolarity = TIM_OCPolarity_High;输出极性来定的。
 	有效电平加输出极性就等于什么时候输出高电
 	*/ 
-	TIM_OCInitStructure.TIM_OCMode 		= TIM_OCMode_PWM2; 			
+	TIM_OCInitStructure.TIM_OCMode 		= TIM_OCMode_PWM1; 			
  	TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable; 	
-	TIM_OCInitStructure.TIM_OCPolarity  = TIM_OCPolarity_Low;	
+	TIM_OCInitStructure.TIM_OCPolarity  = TIM_OCPolarity_High;	
 
 	/*TIM1，TIM8必须使用，否则在RTOS中无法使用，暂时不了解原因*/
 	TIM_OCInitStructure.TIM_OutputNState = TIM_OutputNState_Disable;	/* only for TIM1 and TIM8. */	
@@ -88,8 +99,10 @@ static void bsp_InitTimer1(uint16_t arr,uint16_t psc)
 	/*TIM1，TIM8必须使用，其他定时器可使用或不使用*/
 	TIM_CtrlPWMOutputs(TIM1,ENABLE);
 	
-	TIM_SetCompare1(TIM1,1000);
-	TIM_SetCompare2(TIM1,5000);
-
+	TIM_SetCompare1(TIM1,3600/4);
+	TIM_SetCompare2(TIM1,3600/3);
+	TIM_SetCompare3(TIM1,3600/2);
+	TIM_SetCompare4(TIM1,3600);
+	
 }
 
