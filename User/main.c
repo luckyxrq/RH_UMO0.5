203136ea7ce9391546939dfcab112be90c7431ad
luckyxrq <luckyxrq@163.com>
2019-08-19 18:04:05 +0800

    加入位置信息，准备整理位置信息进结构体，周期性调用

--- User/main.c
+++ User/main.c
@@ -216,6 +216,53 @@ static void vTaskStart(void *pvParameters)
 //		bsp_SearchChargingPileAct();
 		
         vTaskDelay(1);
+		
+		
+		int32_t leftCurrentSpeed  = 0 ;
+		int32_t rightCurrentSpeed = 0 ;
+		
+		int32_t leftLastSpeed  = 0 ;
+		int32_t rightLastSpeed = 0 ;
+		
+		double lastSpeed  = 0 ;
+		double currentSpeed = 0 ;
+		
+		int32_t currentX = 0;
+		int32_t currentY = 0;
+		
+		int32_t lastX = 0;
+		int32_t lastY = 0;
+		
+		uint32_t currentTimestamp = 0;
+		uint32_t lastTimestamp = 0;
+		
+		double last_orientation = 0;
+		double current_orientation = 0;
+		
+		currentTimestamp = xTaskGetTickCount();
+		{//per 20ms
+			
+			lastSpeed = currentSpeed;
+			lastTimestamp = currentTimestamp; 
+			last_orientation = current_orientation;
+			lastX = currentX;
+			lastY = currentY;
+			
+			currentTimestamp = xTaskGetTickCount();
+			leftCurrentSpeed  = bsp_MotorGetSpeed(MotorLeft);
+			rightCurrentSpeed = bsp_MotorGetSpeed(MotorRight);
+			currentSpeed = (leftCurrentSpeed + rightCurrentSpeed) *0.5F*0.001 ; //m/s
+			current_orientation = Deg2Rad(bsp_AngleReadRaw()*0.01f);
+			
+			
+			
+			currentX = lastX + (currentSpeed*cos(current_orientation)+lastSpeed*cos(last_orientation))*(currentTimestamp-lastTimestamp)*0.0005f;
+			currentY = lastY + (currentSpeed*sin(current_orientation)+lastSpeed*sin(last_orientation))*(currentTimestamp-lastTimestamp)*0.0005f;
+			
+			
+			
+		}   
+		
     }
 }
 				
