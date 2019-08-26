9495cfca2a018906661c75d82fce5707692470c7
luckyxrq <luckyxrq@163.com>
2019-08-26 11:20:07 +0800

    增加清扫电机，吸尘器

--- User/app/inc/bsp_motor.h
+++ User/app/inc/bsp_motor.h
@@ -13,12 +13,35 @@ typedef enum
 
 typedef enum
 {
+	MotorRollingBrush = 0 ,
+	MotorSideBrush
+}MotorCleanSN;
+
+typedef enum
+{
 	Forward = 0 ,
 	Backward
 }MotorDir;
 
+typedef enum
+{
+	CW = 0 ,
+	CCW
+}MotorCleanDir;
+
+typedef struct
+{
+	volatile bool isRunning;
+	volatile uint32_t tick;
+}Vacuum;
+
 void bsp_InitMotor(void);
 void bsp_MotorSetPWM(MotorSN sn ,MotorDir dir,uint16_t pwm);
+void bsp_MotorCleanSetPWM(MotorCleanSN sn, MotorCleanDir dir , uint16_t pwm);
+
+void bsp_VacuumClean(void);
+void bsp_StartVacuum(void);
+void bsp_StopVacuum(void);
 
 #endif
 
