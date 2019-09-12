3b1018945a61192e4a0a5791ca464373feadb076
luckyxrq <luckyxrq@163.com>
2019-09-12 14:24:13 +0800

    协助红外判断碰撞方向，基础功能函数

--- User/app/inc/bsp_assistJudgeDirection.h
+++ User/app/inc/bsp_assistJudgeDirection.h
@@ -1,4 +1,8 @@
 #ifndef __BSP_ASSISTJUDGEDIRECTION_H
 #define __BSP_ASSISTJUDGEDIRECTION_H
 
+void bsp_StartAssistJudgeDirection(void);
+void bsp_StopAssistJudgeDirection(void);
+void bsp_AssistJudgeDirection(void);
+
 #endif
--- User/app/src/bsp_assistJudgeDirection.c
+++ User/app/src/bsp_assistJudgeDirection.c
@@ -1,2 +1,90 @@
 #include "bsp.h"
 
+#define HISTORICAL_RECORD_COUNT      10
+#define THRESHOLD_OBSTACLE           1.0F
+
+typedef struct
+{
+	bool isRunning;
+	uint32_t delay;
+	uint32_t action;
+	
+	bool historyRemote[HISTORICAL_RECORD_COUNT];
+	uint32_t historyIndex;
+}AssistJudgeDirection;
+
+static AssistJudgeDirection assistJudgeDirection;
+
+/*
+*********************************************************************************************************
+*	函 数 名: bsp_StartAssistJudgeDirection
+*	功能说明: 开启协助判断是否前面碰撞。
+*	形    参: 无
+*	返 回 值: 无
+*   优 先 级: 4  
+*********************************************************************************************************
+*/
+void bsp_StartAssistJudgeDirection(void)
+{
+	assistJudgeDirection.action = 0 ;
+	assistJudgeDirection.delay = 0 ;
+	assistJudgeDirection.isRunning = 0 ;
+	assistJudgeDirection.historyIndex = 0 ;
+	memset(assistJudgeDirection.historyRemote,0,HISTORICAL_RECORD_COUNT);
+	
+	assistJudgeDirection.isRunning = true;
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: bsp_StopAssistJudgeDirection
+*	功能说明: 停止协助判断是否前面碰撞。
+*	形    参: 无
+*	返 回 值: 无
+*   优 先 级: 4  
+*********************************************************************************************************
+*/
+void bsp_StopAssistJudgeDirection(void)
+{
+	assistJudgeDirection.isRunning = false;
+	
+	assistJudgeDirection.action = 0 ;
+	assistJudgeDirection.delay = 0 ;
+	assistJudgeDirection.isRunning = 0 ;
+	assistJudgeDirection.historyIndex = 0 ;
+	memset(assistJudgeDirection.historyRemote,0,HISTORICAL_RECORD_COUNT);
+}
+
+/*
+*********************************************************************************************************
+*	函 数 名: bsp_AssistJudgeDirection
+*	功能说明: 协助判断是否是前面碰撞。
+*	形    参: 无
+*	返 回 值: 无
+*   优 先 级: 4  
+*********************************************************************************************************
+*/
+void bsp_AssistJudgeDirection(void)
+{
+	if(!assistJudgeDirection.isRunning)
+		return;
+	
+	/*前面红外的状态*/
+	if(bsp_GetInfraRedAdcVoltage(IR2) >= THRESHOLD_OBSTACLE || bsp_GetInfraRedAdcVoltage(IR3) >= THRESHOLD_OBSTACLE || bsp_GetInfraRedAdcVoltage(IR4) >= THRESHOLD_OBSTACLE)
+	{
+		assistJudgeDirection.historyRemote[assistJudgeDirection.historyIndex] = true;
+	}
+	else
+	{
+		assistJudgeDirection.historyRemote[assistJudgeDirection.historyIndex] = false;
+	}
+	
+	
+	/*序号计数*/
+	assistJudgeDirection.historyIndex++;
+	if(assistJudgeDirection.historyIndex >= HISTORICAL_RECORD_COUNT)
+	{
+		assistJudgeDirection.historyIndex = 0 ;
+	}
+}
+
