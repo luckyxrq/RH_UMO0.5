ab8f95d0203dfb1640c07aa3ff8f8ad6de76640b
guohaodong <guohaodong@live.com>
2020-04-17 16:44:13 +0800

    newest gridmap strategy

--- User/main.c
+++ User/main.c
@@ -556,9 +556,9 @@ static void bsp_KeyProc(void)
 					//bsp_StartCliffTest();
 					/*¿ªÇåÉ¨²ßÂÔ*/
 					bsp_StartUpdateCleanStrategyB();
-					bsp_StartVacuum();
-					bsp_MotorCleanSetPWM(MotorRollingBrush, CCW , CONSTANT_HIGH_PWM*0.9F);
-					bsp_MotorCleanSetPWM(MotorSideBrush, CW , CONSTANT_HIGH_PWM*0.7F);
+//					bsp_StartVacuum();
+//					bsp_MotorCleanSetPWM(MotorRollingBrush, CCW , CONSTANT_HIGH_PWM*0.9F);
+//					bsp_MotorCleanSetPWM(MotorSideBrush, CW , CONSTANT_HIGH_PWM*0.7F);
 					
 					//bsp_StartEdgewiseRun();
 					
