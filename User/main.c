db679395ba02238b9824e5a4ac86fb31f523846b
luckyxrq <luckyxrq@163.com>
2020-04-20 23:34:25 +0800

    昊东更改的

--- User/main.c
+++ User/main.c
@@ -129,6 +129,12 @@ static void vTaskDecision(void *pvParameters)      //决策 整机软件控制流程
 		
 #if 1 /*更新地图*/
 		
+		if(isSearchCharge){}
+		else{		
+			bsp_GridMapUpdate(bsp_GetCurrentPosX(),bsp_GetCurrentPosY(),bsp_GetCurrentOrientation(),bsp_CollisionScan(),bsp_GetIRSensorData(),bsp_GetCliffSensorData());
+
+		}
+		
 		//DEBUG("Start:%d\r\n",xTaskGetTickCount());
 		//DEBUG("X:%d,Y:%d#\n",bsp_GetCurrentPosX(),bsp_GetCurrentPosY());
 		//DEBUG("End:%d\r\n",xTaskGetTickCount());
@@ -140,15 +146,7 @@ static void vTaskDecision(void *pvParameters)      //决策 整机软件控制流程
 		}
 		
 		//DEBUG("bsp_GetKeyRunLastState:%d\r\n",bsp_GetKeyRunLastState());
-		if(isSearchCharge){}
-		else{		
-			
-			bsp_UpdateCleanStrategyB(bsp_GetCurrentPosX(),bsp_GetCurrentPosY(),bsp_GetCurrentOrientation(), bsp_CollisionScan(), \
-			bsp_MotorGetPulseVector(MotorLeft), bsp_MotorGetPulseVector(MotorRight), bsp_GetIRSensorData(),bsp_GetCliffSensorData());
-			
-			
 		
-		}
 		//bsp_UploadMap();
         vTaskDelay(50);	
     }
@@ -185,9 +183,12 @@ static void vTaskControl(void *pvParameters)       //控制 根据决策控制电机
 		
 		if(count %2 ==0)
 		{
-			//bsp_UpdateCleanStrategyB(bsp_GetCurrentPosX(),bsp_GetCurrentPosY(),bsp_GetCurrentOrientation(), bsp_CollisionScan(), \
-			bsp_MotorGetPulseVector(MotorLeft), bsp_MotorGetPulseVector(MotorRight), bsp_GetIRSensorData(),bsp_GetCliffSensorData());
-			//DEBUG("%+4d,%+4d#%+3d \n",bsp_GetCurrentPosX()/10,bsp_GetCurrentPosY()/10,(int)Rad2Deg(bsp_GetCurrentOrientation()));
+			if(isSearchCharge){}
+			else{	
+				bsp_UpdateCleanStrategyB(bsp_GetCurrentPosX(),bsp_GetCurrentPosY(),bsp_GetCurrentOrientation(), bsp_CollisionScan(), \
+				bsp_MotorGetPulseVector(MotorLeft), bsp_MotorGetPulseVector(MotorRight), bsp_GetIRSensorData(),bsp_GetCliffSensorData());
+				
+			}//DEBUG("%+4d,%+4d#%+3d \n",bsp_GetCurrentPosX()/10,bsp_GetCurrentPosY()/10,(int)Rad2Deg(bsp_GetCurrentOrientation()));
 		}
 		
 		if(GetReturnChargeStationStatus())
@@ -197,7 +198,8 @@ static void vTaskControl(void *pvParameters)       //控制 根据决策控制电机
 			bsp_PutKey(KEY_LONG_CHARGE);
 		}
 		
-		bsp_GridMapUpdate(bsp_GetCurrentPosX(),bsp_GetCurrentPosY(),bsp_GetCurrentOrientation(),bsp_CollisionScan(),bsp_GetIRSensorData(),bsp_GetCliffSensorData());
+		
+		
 		
 		count++;
         vTaskDelay(10);
