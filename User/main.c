608c97ca8fd93f9f74f3dd51bb8689cc6c4225f2
luckyxrq <luckyxrq@163.com>
2020-05-18 11:57:55 +0800

    火神

--- User/main.c
+++ User/main.c
@@ -6,7 +6,7 @@
 **********************************************************************************************************
 */
 #define PAUSE_INTERVAL_RESPONSE_TIME         1
-#define AT_POWER_ON_OPEN_ALL_MODULE_EN       0     /*在开机的时候直接打开所有的电机轮子...，用于调试的时候使用*/
+#define AT_POWER_ON_OPEN_ALL_MODULE_EN       1     /*在开机的时候直接打开所有的电机轮子...，用于调试的时候使用*/
 
 #define DEBUG_CLOSE_CLEAN_MOTOR              0 //1 关闭清扫电机
 #define main_debug(format, ...) //printf (format, ##__VA_ARGS__)
@@ -141,7 +141,7 @@ static void vTaskDecision(void *pvParameters)      //决策 整机软件控制流程
     {
         /* 处理按键事件 */
 		main_debug("bsp_KeyProc() \n");
-        bsp_KeyProc();
+        //bsp_KeyProc();
 		
 		
         if(count++ % 10 == 0)
@@ -330,7 +330,7 @@ static void vTaskPerception(void *pvParameters)
 		
 		/*检测主机悬空*/
 		//main_debug("bsp_OffSiteProc() \n");
-		bsp_OffSiteProc();
+		//bsp_OffSiteProc();
         /*寻找充电桩*/
 		//main_debug("bsp_SearchChargePile() \n");
 		bsp_SearchChargePile();
