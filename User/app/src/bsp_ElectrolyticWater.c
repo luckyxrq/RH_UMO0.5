85e8fe286db48f77abbfeced22e482d54385c052
luckyxrq <luckyxrq@163.com>
2020-05-18 17:43:08 +0800

    电解水

--- User/app/src/bsp_ElectrolyticWater.c
+++ User/app/src/bsp_ElectrolyticWater.c
@@ -35,3 +35,96 @@ void bsp_InitElectrolyticWater(void)
 	ELECTROLYTIC_WATER_OFF();
 }
 
+
+typedef struct
+{
+	bool isRunning;
+	uint32_t delay;
+	uint32_t action;
+	
+	uint32_t cnt;
+	uint32_t onUS;
+}ElectrolyticWater;
+
+
+static ElectrolyticWater electrolyticWater;
+
+
+static inline void bsp_ELECTROLYTIC_WATER_ON(void);
+static inline void bsp_ELECTROLYTIC_WATER_OFF(void);
+
+
+static inline void bsp_ELECTROLYTIC_WATER_ON(void)
+{
+	
+	if(!electrolyticWater.isRunning)
+	{
+		ELECTROLYTIC_WATER_OFF();
+		return;
+	}
+	
+	/*2MS  一个周期   0-5S  35US开*/
+	if(electrolyticWater.cnt <= (5000 / 2))
+	{
+		electrolyticWater.onUS = 35 ;
+	}
+	/*2MS  一个周期   5-8S  35US开*/
+	else if(electrolyticWater.cnt > (5000 / 2) && electrolyticWater.cnt <= (8000 / 2))
+	{
+		
+	}
+	
+	ELECTROLYTIC_WATER_ON();
+	
+	bsp_StartHardTimer(1, electrolyticWater.onUS, (void *)bsp_ELECTROLYTIC_WATER_OFF);
+	
+	
+}
+
+static inline void bsp_ELECTROLYTIC_WATER_OFF(void)
+{
+	if(!electrolyticWater.isRunning)
+	{
+		ELECTROLYTIC_WATER_OFF();
+		return;
+	}
+	
+	
+	ELECTROLYTIC_WATER_OFF();
+	
+	bsp_StartHardTimer(1, 2000-electrolyticWater.onUS, (void *)bsp_ELECTROLYTIC_WATER_ON);
+	
+	/*2MS  一个周期   前面5S  35US开*/
+	/*2MS  一个周期   3S内  35US开变为760US开*/
+	++electrolyticWater.cnt;
+}
+
+
+void bsp_StartElectrolyticWaterProc(void)
+{
+	electrolyticWater.action = 0 ;
+	electrolyticWater.delay = 0 ;
+	electrolyticWater.cnt = 0 ;
+	electrolyticWater.isRunning = true;
+	
+	bsp_ELECTROLYTIC_WATER_ON();
+}
+
+
+void bsp_ElectrolyticWaterProc(void)
+{
+	if(!electrolyticWater.isRunning)
+		return;
+	
+	
+	switch(electrolyticWater.action)
+	{
+		case 0:
+		{
+			
+		}break;
+	}
+}
+
+
+
