6a6f5913508d96bb24fb44cb8e54f03223b1d2a1
guohaodong <guohaodong@live.com>
2020-08-27 11:01:26 +0800

    关闭数据上传，周五发布这个版本

--- User/main.c
+++ User/main.c
@@ -8,7 +8,7 @@
 #define PAUSE_INTERVAL_RESPONSE_TIME         1
 #define AT_POWER_ON_OPEN_ALL_MODULE_EN       0     /*在开机的时候直接打开所有的电机轮子...，用于调试的时候使用*/
 #define DEBUG_CLOSE_CLEAN_MOTOR              0 //1 关闭清扫电机
-#define DEBUG_STRATEGY_SHOW                  1
+#define DEBUG_STRATEGY_SHOW                  0
 /*
 **********************************************************************************************************
                                             函数声明
